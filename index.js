038aea3a0ecf678636ece138ddaaab9c50c78c29
Arjun Sabu <63174870+ARJUN-SABU@users.noreply.github.com>
2020-12-05 13:45:23 +0530

    initial Game website files

---
+++ index.js
@@ -0,0 +1,18 @@
+document.querySelector(".burger").addEventListener("click", function(){
+
+    document.querySelector(".navbar").classList.toggle("h-nav-resp");
+    document.querySelector(".leftNav").classList.toggle("v-nav-resp");
+    document.querySelector(".rightNav").classList.toggle("v-nav-resp");
+
+
+})
+
+
+// document.querySelector(".burger").addEventListener("touchstart", function(){
+//
+//     document.querySelector(".navbar").classList.toggle("h-nav-resp");
+//     document.querySelector(".leftNav").classList.toggle("v-nav-resp");
+//     document.querySelector(".rightNav").classList.toggle("v-nav-resp");
+//
+//
+// })
