57d75c50b0876303f7986a56a9f082ff0234834a
Arjun Sabu <63174870+ARJUN-SABU@users.noreply.github.com>
2020-12-05 13:51:28 +0530

    js file removed unnecessary comments

--- index.js
+++ index.js
@@ -8,11 +8,3 @@ document.querySelector(".burger").addEventListener("click", function(){
 })
 
 
-// document.querySelector(".burger").addEventListener("touchstart", function(){
-//
-//     document.querySelector(".navbar").classList.toggle("h-nav-resp");
-//     document.querySelector(".leftNav").classList.toggle("v-nav-resp");
-//     document.querySelector(".rightNav").classList.toggle("v-nav-resp");
-//
-//
-// })
